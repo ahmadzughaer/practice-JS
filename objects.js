09549ae7fae5c131edaddd042770daa6552b87f9
ahmadzughaer <ahmadzughaer@gmail.com>
2022-07-11 12:50:52 +0300

    JS objects

---
+++ objects.js
@@ -0,0 +1,11 @@
+// declare object 
+
+const apartment = {
+    imgUrl: "https://via.placeholder.com/640x480",
+    descr: "Spacious apartment in the city center",
+    rating: 4,
+    price: 2153,
+    tags: ["premium", "promoted", "top"]
+};
+
+// 
